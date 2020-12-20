15cdfaf94624050dcd59066972dc4a9f06fd8d94
MJHyuk <shekwl24@gmail.com>
2020-12-21 07:43:46 +0900

    delete the post   - 삭제 버튼은 board 작성자와 일치하지 않으면 hidden으로 표시   - delete가 예약어로 이미 존재하여 deleteById로 함수명 작성함   - delete와 update가 같은 api로 전송하지만 각각 DELETE, PUT으로 요청방식이 다르기 떄문에 문제없음
    modify the post
      - 글 수정 시 기존의 작성 제목, 내용 출력
      - 수정 UI는 이전의 글 작성 UI를 그대로 가져옴
      - JPA에서는 트랜잭션 종료 시 더티체킹을 하기때문에 BoardService에서 글수정하기() 함수는 따로 save()가 필요없음
        -> findById(id)를 이용하여 먼저 영속화를 시킨다음 글 제목, 내용을 수정하면 트랜잭션 종료 시 자동으로 update됨 (dirty checking)

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,7 +1,13 @@
 let index = {
 	init: function() {
-		$("#btn-save").on("click", () => { 
+		$("#btn-save").on("click", () => {
 			this.save();
+		});
+		$("#btn-delete").on("click", () => {
+			this.deleteById();
+		});
+		$("#btn-update").on("click", () => {
+			this.update();
 		});
 	},
 
@@ -14,16 +20,53 @@ let index = {
 		$.ajax({
 			type: "POST",
 			url: "/api/board",
-			data: JSON.stringify(data), 
-			contentType: "application/json; charset=utf-8",  
-			dataType: "json" 
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
 		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
-		}); 
-	}
+		});
+	},
+
+	deleteById: function() {
+		let id = $("#id").text();
+
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/" + id,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	update: function() {
+		let id = $("#id").val();
+	
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+
+		$.ajax({
+			type: "PUT",
+			url: "/api/board/" + id,
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("글수정이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
 }
 
 index.init();
