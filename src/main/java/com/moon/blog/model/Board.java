2f5a972bde4f8899b9ce84c903d49cdd3ec3076a
MJHyuk <shekwl24@gmail.com>
2020-12-25 06:39:52 +0900

    join error handle   - username이 같아 회원가입이 안되는 등 500상태 에러발생 시 처리
    add cascade attribute to replys
      - 글 삭제 시 댓글도 함께 삭제되도록 함

--- src/main/java/com/moon/blog/model/Board.java
+++ src/main/java/com/moon/blog/model/Board.java
@@ -3,6 +3,7 @@ package com.moon.blog.model;
 import java.sql.Timestamp;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -48,7 +49,7 @@ public class Board {
 	@JoinColumn(name="userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바 오브젝트를 저장할 수 있다.
 	
-	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다. (난 FK가 아니다) DB에 컬럼을 만들지 말라
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE) // mappedBy 연관관계의 주인이 아니다. (난 FK가 아니다) DB에 컬럼을 만들지 말라
 	@JsonIgnoreProperties({"board"})
 	@OrderBy("id desc")
 	private List<Reply> replys;
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -26,9 +26,13 @@ let index = {
 			contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME) 
 			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript 오브젝트로 변경
 		}).done(function(resp) {
-			alert("회원가입이 완료되었습니다.");
-			//console.log(resp);
-			location.href = "/";
+			if(resp.status === 500) {
+				alert("회원가입이 실패하였습니다.");
+			} else {
+				alert("회원가입이 완료되었습니다.");
+				location.href = "/";
+			}
+
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
